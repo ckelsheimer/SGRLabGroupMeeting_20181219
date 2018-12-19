8f264cffb871c6cb4ea5a7d9293ec055465cabc1
CJ Kelsheimer <cjk85@pitt.edu>
2018-12-19 15:08:08 -0500

    kai gm updates

--- ObjectOrientedWorkspaces/Cat.m
+++ ObjectOrientedWorkspaces/Cat.m
@@ -1,18 +1,36 @@
 classdef Cat < handle
     
-    properties
+    properties(SetAccess=private)
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %       Put your code below      %
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        
+        name;
+        mood;
+        hungry;
+        energy;
     end
     
-    methods
+    methods(Static,Access = private)
+        function meow()
+            fprintf('Meow!\n');
+        end
+    end
+    methods(Access = public)
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %       Put your code below      %
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        function obj=Cat(name,mood,hungry,energy)
+            obj.name = name;
+            obj.mood = mood;
+            obj.hungry=hungry;
+            obj.energy=energy;
+        end
         
+        function obj=Feed(obj)
+            obj.hungry = obj.hungry - 1;
+            obj.mood = obj.mood+1;
+            obj.meow;
+        end
     end
+    
 end
-        
-        
